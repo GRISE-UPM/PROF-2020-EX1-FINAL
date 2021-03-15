eaaaec7fa2f885c6000321730aa7ed6c94a6a619
odieste <odieste@fi.upm.es>
2021-03-15 17:05:50 +0100

    ejercicio resuelto

--- src/main/java/es/upm/grise/ex1final2020/Key.java
+++ src/main/java/es/upm/grise/ex1final2020/Key.java
@@ -3,12 +3,18 @@ package es.upm.grise.ex1final2020;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+// Tenemos que incluir la clase Time por inyección de dependencia.
+//
+// La solución no me gusta demasiado porque los elementos estáticos son siempre problemáticos. Pero funciona.
+
 public class Key {
+
+	static Time time;
 
 	public static String getKey() {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss-SSS");
-		LocalDateTime time = Time.getCurrentTime();
-		return formatter.format(time);
+		LocalDateTime curremtTime = time.getCurrentTime();
+		return formatter.format(curremtTime);
 	}	
 	
 }
--- src/main/java/es/upm/grise/ex1final2020/Time.java
+++ src/main/java/es/upm/grise/ex1final2020/Time.java
@@ -2,11 +2,15 @@ package es.upm.grise.ex1final2020;
 
 import java.time.LocalDateTime;
 
+// Se podria hacer de muchas formas que Time participase en pruebas de interacción.
+// Fijate que es lo mismo que se persigue con diseño testable
+// Mi solución es Eliminar la invocación estática de getCurrentTime(). Con mockito después no hace falta nada más
+
 public class Time {
-	
-		public static LocalDateTime getCurrentTime () {
-			LocalDateTime time = LocalDateTime.now();
-			return time;	
-		}
+
+	public LocalDateTime getCurrentTime () {
+		LocalDateTime time = LocalDateTime.now();
+		return time;	
+	}
 
 }
